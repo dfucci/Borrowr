f5e8f161f57c1f26d27e6cc323354be830da4276
dfucci <davfuc@gmail.com>
2016-10-27 17:06:26 +0300

    first commit

--- app/components/Home.js
+++ app/components/Home.js
@@ -9,9 +9,37 @@ export default class Home extends Component {
       <div>
         <div className={styles.container}>
           <h2>Home</h2>
+          <BookList />
           <Link to="/counter">to Counter</Link>
         </div>
       </div>
     );
+  }
+}
+
+class BookList extends Component {
+  render(){
+    return(
+      <table>
+      <tbody>
+      <tr>
+      <th>Firstname</th>
+      <th>Lastname</th>
+      <th>Age</th>
+      </tr>
+      <tr>
+      <td>Jill</td>
+      <td>Smith</td>
+      <td>50</td>
+      </tr>
+      <tr>
+      <td>Eve</td>
+      <td>Jackson</td>
+      <td>94</td>
+      </tr>
+      </tbody>
+      </table>
+
+    )
   }
 }
