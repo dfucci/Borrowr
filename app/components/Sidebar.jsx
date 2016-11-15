f60c9f4d3aa1b6e8fce3db48282fcd115cf372b2
dfucci <davfuc@gmail.com>
2016-11-15 22:34:25 +0100

    Added sidebar

---
+++ app/components/Sidebar.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Pane, NavGroup, NavTitle, NavGroupItem } from "react-photonkit";
+
+class Sidebar extends React.Component {
+  onSelect(index) {
+    console.log(`sidebar clicked with ${index}`)
+  }
+
+  render() {
+    return (
+      <Pane ptSize="sm" sidebar>
+        <NavGroup activeKey={1} onSelect={this.onSelect}>
+          <NavGroupItem eventKey={1} glyph="home" text="home" />
+          <NavGroupItem eventKey={2} glyph="download" text="download" />
+        </NavGroup>
+      </Pane>
+    );
+  }
+}
+
+export default Sidebar;
